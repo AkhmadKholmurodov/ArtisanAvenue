83d3fca67ea60d384d2c41ebe75b53c28c217b55
Akhmad Kholmurodov <akhmadkholmurodov@Akhmads-MacBook-Air.local>
2023-11-05 11:16:24 +0900

    I reuploaded after hosting

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  basePath: '/your-repository-name', // Replace with your GitHub repository name
+  assetPrefix: '/your-repository-name/', // Replace with your GitHub repository name
+}
+const nextConfig = {
+	images: {
+		domains: ['fakestoreapi.com'],
+	},
+};
+
+module.exports = nextConfig;
